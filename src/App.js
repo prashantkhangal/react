d6f867d50a65163686bf4efc52481f4065ceb664
prashantkhangal <prashantkhangal901@gmail.com>
2022-05-20 20:12:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import FunctionalComponent from './practice/FunctionalComponent';
+import ClassComponent from './practice/ClassComponent';
+import './practice/StylePrac.css'
+import { useState } from 'react';
+function App() {
+  const[name1,setName1]=useState(false);
+  const[name2,setName2]=useState(false);
+
+    return (
+    <div className="App">
+      <h1 style={{fontSize:"3vw"}}>Style Using Function Component and Class Component</h1>
+      <div className="hid">
+      <header1>  
+      <button onClick={()=>{name1?setName1(false):setName1(true)}}>To Style with Functional Component</button>
+      {name1&&<FunctionalComponent />}
+      </header1>
+
+      <header2>
+      <button onClick={()=>{name2?setName2(false):setName2(true)}} >To Style with Class Component</button>
+      {name2&&<ClassComponent />}
+      </header2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/practice/FunctionalComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FunctionalComponent = () => {
+  return (
+     <div id="boxFunc">
+          <h3 style={{marginTop:"1vw",marginBottom:"1vw"}}>I was made using Functional Component</h3>
+          <p>This is done by using external CSS</p>
+          <p style={{color:"red"}}>This is done by using internal CSS</p>
+     </div>
+  )
+}
+
+export default FunctionalComponent
